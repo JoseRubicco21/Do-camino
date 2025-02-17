21f514d3f01d806b728e72ca507711d891caeef3
a23joserf <a23joserf@iessanclemente.net>
2025-02-17 10:43:34 +0100

    Fixed the problem with links & js

--- assets/js/cartas.js
+++ assets/js/cartas.js
@@ -1,20 +1,21 @@
-// Este archivo hace que desde la pagina principal, al darle a VER MENU, 
+// Este archivo hace que desde la pagina principal, al darle a VER MENU,
 // nos lleva a otra pagina desplegando la carta de forma automatica
 
 document.addEventListener('DOMContentLoaded', function () {
-    // Obtén el hash de la URL (por ejemplo, "#flush-collapseOne")
-    let hash = window.location.hash;
-
-    // Si hay un hash en la URL
-    if (hash) {
-        // Selecciona el elemento del acordeón usando el hash
-        let targetCollapse = document.querySelector(hash);
-
-        // Si el elemento existe, despliégalo usando Bootstrap
-        if (targetCollapse && targetCollapse.classList.contains('custom-button')) {
-            let collapse = new bootstrap.Collapse(targetCollapse, {
-                toggle: true // Esto fuerza el despliegue
-            });
-        }
-    }
+	// Obtén el hash de la URL (por ejemplo, "#flush-collapseOne")
+	let hash = window.location.hash;
+	console.log(hash);
+	// Si hay un hash en la URL
+	if (hash) {
+		// Selecciona el elemento del acordeón usando el hash
+		let targetCollapse = document.querySelector(hash);
+		console.log(targetCollapse);
+		// Si el elemento existe, despliégalo usando Bootstrap
+		if (targetCollapse) {
+			console.log('Should collapse');
+			let collapse = new bootstrap.Collapse(targetCollapse, {
+				toggle: true, // Esto fuerza el despliegue
+			});
+		}
+	}
 });
